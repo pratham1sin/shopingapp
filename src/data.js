b427820eb56014a1310a9966e457f504628ded88
“pratham <spratham906@gmail.com>
2023-10-23 09:17:50 +0530

    inisal commit

---
+++ src/data.js
@@ -0,0 +1,89 @@
+
+
+export const products = [
+  {
+    id:1,
+    image: "https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    title:toyata ,
+    year:2021,
+    fule: hybird,
+    speed:km,
+    price: 440,
+
+    
+  },
+  {
+    id:2,
+    image: "https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    title:BMW3Series ,
+    year:2019,
+    fule: gasoline,
+    speed:km,
+    price: 350,
+
+    
+  },
+  {
+    id:3,
+    image: "https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    title:VolkswagenT-Cross ,
+    year:2020,
+    fule: gasoline,
+    speed:km,
+    system:Automatic,
+    price: 400,
+
+    
+  },
+  {
+    id:4,
+    image: "https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    title:cadilacEscalade ,
+    year:2020,
+    fule: hybird,
+    speed:km,
+    system:Automatic,
+    price: 440,
+
+    
+  },
+  {
+    id:5,
+    image: "https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    title:BMW4serice,
+    year:2021,
+    fule: hybird,
+    speed:km,
+    system:Automatic,
+    price: 470,
+
+    
+  },
+  {
+    id:6,
+    image: "https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    image1:"https://cars.usnews.com/cars-trucks/toyota/rav4/photos-exterior",
+    title:nano ,
+    year:2019,
+    fule: hybird,
+    speed:km,
+    system:mannul,
+    price: 440,
+
+    
+  },
+  
+  
+  
+];
